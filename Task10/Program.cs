578cbdf30613c05d506474d1a79164b91de7300c
Anna <adjikiss@mail.ru>
2022-03-13 17:58:39 +0300

    Task 10 added (second attempt)

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 10: Напишите программу, которая принимает на вход
+ трёхзначное число и на выходе показывает вторую цифру этого числа.
+ Примеры: 456 -> 5       782 -> 8        918 -> 1*/
+
+ int FindSecondNum(int num)
+ {
+    int result = (num / 10) % 10;
+    return result;
+ }
+
+Console.WriteLine("Введите целое трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Вторая цифра числа - " + FindSecondNum(num));
